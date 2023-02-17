bb0df998ca05a15fc0070a52c6116b0112ada476
Антон <lopatuxin.a@gmail.com>
2023-02-17 20:13:45 +0300

    2. Создание и запуск нити [#1016]

---
+++ src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
@@ -0,0 +1,14 @@
+package main.java.ru.job4j.concurrent;
+
+public class ConcurrentOutput {
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        first.start();
+        second.start();
+    }
+}
