afce38dc52e8fd35747711f05932d394b9b0c052
Антон <lopatuxin.a@gmail.com>
2023-02-20 12:41:35 +0300

    8. Скачивание файла с ограничением. [#144271]

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -8,21 +8,33 @@ import java.net.URL;
 public class Wget implements Runnable {
     private final String url;
     private final int speed;
+    private final String fileName;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, int speed, String fileName) {
         this.url = url;
         this.speed = speed;
+        this.fileName = fileName;
     }
 
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("temp.txt")) {
+             FileOutputStream fileOutputStream = new FileOutputStream(fileName)) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
+            int downloadData = 0;
+            long currentTime = System.currentTimeMillis();
             while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
+                downloadData = downloadData + bytesRead;
+                if (downloadData >= speed) {
+                    long ms = System.currentTimeMillis() - currentTime;
+                    if (ms < 1000) {
+                        Thread.sleep(1000 - ms);
+                        currentTime = System.currentTimeMillis();
+                        downloadData = 0;
+                    }
+                }
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
-                Thread.sleep(dataBuffer.length / speed);
             }
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
@@ -30,12 +42,14 @@ public class Wget implements Runnable {
     }
 
     public static void main(String[] args) throws InterruptedException {
-        if (args.length != 2) {
-            throw new IllegalArgumentException("Args must be 2");
+        args = new String[]{"https://proof.ovh.net/files/10Mb.dat", "1000000", "temp.dat"};
+        if (args.length != 3) {
+            throw new IllegalArgumentException("Args must be 3");
         }
         String url = args[0];
         int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new Wget(url, speed));
+        String fileName = args[2];
+        Thread wget = new Thread(new Wget(url, speed, fileName));
         wget.start();
         wget.join();
     }
