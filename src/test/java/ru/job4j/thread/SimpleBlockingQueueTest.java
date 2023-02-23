0a131632ad62530b316ff8d5a87f259cc0f32e84
Антон <lopatuxin.a@gmail.com>
2023-02-23 19:32:50 +0300

    3. Junit тест для блокирующей очереди. [#68589]

--- src/test/java/ru/job4j/thread/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/thread/SimpleBlockingQueueTest.java
@@ -51,5 +51,46 @@ class SimpleBlockingQueueTest {
         assertThat(lst).isEqualTo(List.of(0, 1, 2, 3, 4, 5, 6, 7, 8 ,9));
     }
 
-
+    @Test
+    public void when2ConsumersAnd1Producer() throws InterruptedException {
+        var queue = new SimpleBlockingQueue<>(2);
+        var producer = new Thread(() -> {
+            for (int i = 0; i < 10; i++) {
+                try {
+                    queue.offer(i);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+        var consumer = new Thread(() -> {
+            for (int i = 0; i < 4; i++) {
+                try {
+                    queue.poll();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+        var consumer1 = new Thread(() -> {
+            for (int i = 0; i < 4; i++) {
+                try {
+                    queue.poll();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+        producer.start();
+        consumer.start();
+        consumer1.start();
+        try {
+            producer.join();
+            consumer.join();
+            consumer1.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        assertThat(queue.poll()).isEqualTo(8);
+    }
 }
